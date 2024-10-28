c547c815f23d35c0762705daa7b6b2ae81687935
Paweł Łęczycki <pawel@msd.info.pl>
2024-10-28 14:20:32 +0100

    Budowa klasy

--- ConsoleApp/Models/Product.cs
+++ ConsoleApp/Models/Product.cs
@@ -1,12 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//namespace - przestrzeń nazw, czyli adres pod którym "mieszka" klasa
+//namespace zaciągamy używając "using"
+using System;
 
 namespace ConsoleApp.Models
 {
+    //public - modyfikator dostępu - oznacza, że z klasy można korzystać wszędzie
+    //internal - modyfikator dostępu - oznacza, że z klasy można korzystać w obrębie projektu
+    //brak modyfikatora dostępu = internal (dla klas) - wybierany jest najniższy poziom dostępu
+    //class - szablon opisujący zachowania i cechy obiektów (instancji klas), które są wytwarzane na jej podstawie
+    //pełna nazwa klasy to <namespace>.<nazwa>
     internal class Product
     {
+        //pole klasy (field)
+        //private - oznacza dostęp tylko dla elementów danej klasy
+        //brak modyfikatora dostępu = private (dla składników klasy)
+        //pola zazwyczaj są prywatne ze względu na hermetyzację, a dostęp realizowany jest przez metody dostępowe (getter i setter)
+        /*private*/ string _name;
+
+        //setter - do ustawiania wartości - metoda przyjmuje parametr, który zostaje wpisany w odpowiednie pole (można dodać kod "obróbki danych")
+        //void - metoda nic nie zwaraca
+        public void SetName(string name)
+        {
+            _name = name.ToUpper();
+        }
+
+        //getter - do pobrania wartości pola - metoda zwraca typ zgodny z typem pola
+        public string GetName()
+        {
+            //instukcja zwracająca wynik działania metody - obowiązkowy gdy zadeklarowaliśmy, że klasa coś zwraca (jest inna niż void)
+            return _name;
+        }
+
+
+        //Property - właściwość
+
+        //auto-property
+        //właściwość integruje w sobie pole i metody dostępowe (getter i setter)
+        //jest możliwość zmiany modyfikatora dostępu dla getter lub setter (osobno)
+        public float Price { /*private*/ get; set; }
+
+
+        //full-property
+
+        //backfield do full-property - pozwala na dodatkowy kod w setterze i getterze
+        private DateTime _expirationDate;
+
+        public DateTime ExpirationDate
+        {           
+            //getter dla property
+            get
+            {
+                return _expirationDate;
+            }
+            //setter dla property - posiada niejawny parametr o nazwie value
+            set
+            {
+                if (value < DateTime.Now)
+                {
+                    _expirationDate = DateTime.Now;
+                }
+                else
+                {
+                    _expirationDate = value;
+                }
+            }
+        }
+
+        public string CreateDescription()
+        {
+            //łączenie stringów za pomocą operatora +
+            string description = GetName() + " (" + ExpirationDate + "): " + Price + "zł";
+
+            string format = "{0} ({1}): {2:f2}zł"; //wartości w nawiasach oznaczają indeks parametru, który ma być wstawiony w to miejsce
+            //łączenie stringów za pomocą funkcji Format
+            description = string.Format(format, GetName(), ExpirationDate, Price);
+
+            //łączenie stringów wykorzystując interpolację (string interpolowany)
+            description = $"{GetName()} ({ExpirationDate}): {Price:f2}zł";
+
+            return description;
+        }
+
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,30 +9,47 @@
 //Instrukcja najwyższego poziomu - instrukcja napisana bezpośrednio w pliku bez klasy i namespace
 //Jest to zawsze punkt startowy programu (main)
 //Dopuszczalny jest tylko jeden plik w projekcie z instrukcjami najwyższego poziomu
+
+
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine(typeof(Product).Name);
+Console.WriteLine(typeof(Product).Namespace);
+Console.WriteLine(typeof(Product).FullName);
 
-int a = 4;
-Sth1(a);
 
-int? c = null;
-Sth1(c);
+Product p1 = new Product();
+p1.SetName("Test");
+Console.WriteLine(p1.GetName());
+p1.Price = 100;
+Console.WriteLine(p1.Price);
+p1.ExpirationDate = DateTime.Now.AddDays(-3);
+Console.WriteLine(p1.ExpirationDate);
 
-object? b = null;
+Console.WriteLine(p1.CreateDescription());
 
-if(b != null)
-    Sth2(b);
+void Nullable() {
+    int a = 4;
+    Sth1(a);
 
-int? Sth1(int? val)
-{
-    return val;
+    int? c = null;
+    Sth1(c);
+
+    object? b = null;
+
+    if(b != null)
+        Sth2(b);
+
+    int? Sth1(int? val)
+    {
+        return val;
+    }
+
+    object Sth2(object obj)
+    {
+        return obj;
+    }
 }
-
-object Sth2(object obj)
-{
-    return obj;
-}
-
 
 //}
 //}
