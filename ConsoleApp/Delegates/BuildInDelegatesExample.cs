36dccee823ef7091426e6e06d333744e2b10f299
Paweł Łęczycki <pawel@msd.info.pl>
2024-10-29 14:50:06 +0100

    Wbudowane delegaty

---
+++ ConsoleApp/Delegates/BuildInDelegatesExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Delegates
+{
+    internal class BuildInDelegatesExample
+    {
+        public void Add(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine(result);
+        }
+
+        public bool Substract(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(result);
+            return result % 2 != 0;
+        }
+
+        public void Test()
+        {
+            Method(Add, Substract);
+        }
+
+        //delegate void Method1Deleagate(int a, int b);
+        //delegate bool Method2Deleagate(int a, int b);
+
+        //void Method(Method1Deleagate method1, Method2Deleagate method2)
+
+        //Action - wbudowana definicja delegata wskazująca na funkcje nie zwracające rezultatu (void). Typ i ilość parametów przekazujemy jako parametry generyczne.
+        //Func - wbudowana definicja delegata wskazująca na funkcje zwracające jakiś rezultat (inne niż void). Typ rezultatu jest ostatnim (lub jedynym) parametrem generycznym.
+        void Method(Action<int, int> method1, Func<int, int, bool> method2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    method1(i, j);
+                    if (method2(i, j))
+                        Console.WriteLine("@@@");
+                }
+            }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -20,6 +20,8 @@ Console.WriteLine( example.Check(CheckModulo, 15, 3) );
 
 new MulticastDelegateExample().Test();
 
+new BuildInDelegatesExample().Test();
+
 //Instrukcja najwyższego poziomu - instrukcja napisana bezpośrednio w pliku bez klasy i namespace
 //Jest to zawsze punkt startowy programu (main)
 //Dopuszczalny jest tylko jeden plik w projekcie z instrukcjami najwyższego poziomu
