64e02792f98e6d8c9516f9216bea0b93a17c7a52
Paweł Łęczycki <pawel@msd.info.pl>
2024-10-30 13:44:10 +0100

    File

--- Services.InFile/EntityInFileService.cs
+++ Services.InFile/EntityInFileService.cs
@@ -48,6 +48,14 @@ namespace Services.InFile
         {
             string json = JsonSerializer.Serialize(Items);
 
+            //File - fasada ułatwiająca pracę z plikami
+            File.WriteAllText(_filePath, json);
+        }
+
+        private void SaveDataUsingStream()
+        {
+            string json = JsonSerializer.Serialize(Items);
+
             //klasy strumieniowe - klasy opierające swoje działanie na strumieniu byte'ów
             //wykorzustanie using spowoduje automatyczne wywołanie funkcji Dispose
             using FileStream fileStream = new FileStream(_filePath, FileMode.Create);
@@ -63,6 +71,14 @@ namespace Services.InFile
         }
 
         private void LoadData()
+        {
+            string json = File.ReadAllText(_filePath);
+
+            List<T> items = JsonSerializer.Deserialize<List<T>>(json)!;
+            Items.AddRange(items);
+        }
+
+        private void LoadDataUsingStream()
         {
             using FileStream fileStream = new FileStream(_filePath, FileMode.OpenOrCreate);
             using StreamReader reader = new StreamReader(fileStream);
